3c207b201ce8aa115b521d95e5b7b249f1e8602a
Onslow-DM <124639667+Onslow-DM@users.noreply.github.com>
2024-06-17 00:06:29 +0000

    testing

--- Program.swift
+++ Program.swift
@@ -4,7 +4,10 @@
 
 import Foundation
 
+let tast = 2
+
 print("Dylan does make cool profile pictures :)")
 print("hello")
 
 //this is a test to sink up things also dan is smell and dumb
+print(test)
