6ab4d6db646399bba7e7e23c8789e79066961a77
KellanVK <kellan.vankoughnet@student.onslow.school.nz>
2024-06-18 14:37:39 +1200

    Set up project for Windows

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "Reversi",
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "Reversi",
+            dependencies: []
+        )
+    ]
+)
