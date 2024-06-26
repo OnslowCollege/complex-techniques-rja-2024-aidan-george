bdab425378112c6f75e85c097b878d25932b69ca
KellanVK <kellan.vankoughnet@student.onslow.school.nz>
2024-06-26 01:13:38 +0000

    Jun 26, 2024, 1:13 PM

---
+++ Sources/TaskList.swift
@@ -0,0 +1,16 @@
+//
+// TastList.swift
+// Subtasks
+//
+
+import Adwaita
+
+struct TaskList: View {
+
+    @Binding var tasks: [Task]
+
+    var view: Body {
+        Text("Hello, world!")
+            .padding()
+    }
+}
