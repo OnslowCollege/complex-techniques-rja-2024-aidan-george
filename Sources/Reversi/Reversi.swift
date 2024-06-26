6c6a08a459861c4e056d78302bb259890f5b6e58
KellanVK <kellan.vankoughnet@student.onslow.school.nz>
2024-06-26 01:23:25 +0000

    Jun 26, 2024, 1:23 PM

--- Sources/Reversi/Reversi.swift
+++ Sources/Reversi/Reversi.swift
@@ -1,23 +1,42 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// // The Swift Programming Language
+// // https://docs.swift.org/swift-book
 
-import Adwaita
+// import Adwaita
 
-@main
-struct Reversi: App {
+// @main
+// struct Reversi: App {
 
-    let id = "nz.school.OnslowCollege.Reversi"
-    var app: GTUIApp!
+//     let id = "nz.school.OnslowCollege.Reversi"
+//     var app: GTUIApp!
 
-    var scene: Scene {
-        Window(id: "main") { window in
-            Text("NotReversi")
-                .padding()
-                .topToolbar {
-                    ToolbarView(app: app, window: window)
-                }
+//     var scene: Scene {
+//         Window(id: "main") { window in
+//             Text("NotReversi")
+//                 .padding()
+//                 .topToolbar {
+//                     ToolbarView(app: app, window: window)
+//                 }
+//         }
+//         .defaultSize(width: 450, height: 300)
+//     }
+
+// }
+struct Counter: View {
+
+    @State private var count = 0
+
+    var view: Body {
+        HStack {
+            Button(icon: .default(icon: .goPrevious)) {
+                count -= 1
+            }
+            Text("\(count)")
+                .title1()
+                .frame(minWidth: 100)
+            Button(icon: .default(icon: .goNext)) {
+                count += 1
+            }
         }
-        .defaultSize(width: 450, height: 300)
     }
 
 }
