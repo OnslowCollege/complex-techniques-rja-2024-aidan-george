44461f1fbb25054f2ec5ab53426621c4d0aecfab
KellanVK <kellan.vankoughnet@student.onslow.school.nz>
2024-06-26 01:09:01 +0000

    Jun 26, 2024, 1:09 PM

--- Sources/Task.swift
+++ Sources/Task.swift
@@ -11,4 +11,10 @@ struct Task {
     var done: Bool = false
     var subtasks: [Self] = []
 
+    var mixed: Bool {
+        !done && sybtasks.contains { subtask in
+            subtask.mixed || subtask.done
+        }
+    }
+
 }
