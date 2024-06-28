b873ded600dad3b914eaec6190a62f17473c8cdf
AidanJWheeler <124639958+AidanJWheeler@users.noreply.github.com>
2024-06-28 05:54:18 +0000

    Jun 28, 2024, 5:54 AM

--- testapp/src/app/home/home.component.ts
+++ testapp/src/app/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HousingLocationComponent],
   template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
     <section class="results">
       <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
     </section>
