ce9c36c295fa8cbe287b750e9d453f8cb60830c3
AidanJWheeler <124639958+AidanJWheeler@users.noreply.github.com>
2024-06-28 05:25:00 +0000

    Jun 28, 2024, 5:25 AM

--- testapp/src/app/app.component.ts
+++ testapp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { KeyboardComponent } from './keyboard/keyboard.component';
+import { DisplayComponent } from './display/display.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, KeyboardComponent],
+  imports: [RouterOutlet, KeyboardComponent, DisplayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
