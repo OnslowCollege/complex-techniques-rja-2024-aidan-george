f59c3f52abb0ce185595f532f86a8e76ec0e8a72
AidanJWheeler <124639958+AidanJWheeler@users.noreply.github.com>
2024-06-28 05:51:18 +0000

    Jun 28, 2024, 5:51 AM

--- testapp/src/app/home/home.component.ts
+++ testapp/src/app/home/home.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
-      </form>
+    <section class="results">
+      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
     </section>
   `,
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
