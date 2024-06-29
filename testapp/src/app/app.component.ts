a260f03c949548c72f1d10597d6add5af113662d
AidanJWheeler <124639958+AidanJWheeler@users.noreply.github.com>
2024-06-29 03:11:51 +0000

    Jun 29, 2024, 3:11 AM

--- testapp/src/app/app.component.ts
+++ testapp/src/app/app.component.ts
@@ -1,25 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HousingLocationComponent],
-  styleUrl: './app.component.scss',
+  imports: [HomeComponent],
   template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
-    <section class="results">
-      <app-housing-location></app-housing-location>
-    </section>
+    <main>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
   `,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+  title = 'homes';
 }
