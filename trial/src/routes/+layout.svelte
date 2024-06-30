20e3a988c91deeb433ffd983d6c652d0db579231
AidanJWheeler <124639958+AidanJWheeler@users.noreply.github.com>
2024-06-30 23:54:56 +0000

    New style

--- trial/src/routes/+layout.svelte
+++ trial/src/routes/+layout.svelte
@@ -2,4 +2,8 @@
     import'/workspaces/complex-techniques-rja-2024-aidan-george.worktrees/Nest-Web-Framework-test/trial/src/app.css'
 </script>
 
+<svelte:head>
+    <title>Matching Game</title>
+</svelte:head>
+
 <slot />
