78536045a904b5f62438aef1055b8fb9519591ee
KellanVK <kellan.vankoughnet@student.onslow.school.nz>
2024-06-30 23:34:00 +0000

    Jul 1, 2024, 11:34 AM

--- Sources/Reversi/Reversi.swift
+++ Sources/Reversi/Reversi.swift
@@ -1,23 +1,70 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// // The Swift Programming Language
+// // https://docs.swift.org/swift-book
+
+// import Adwaita
+
+// @main
+// struct Reversi: App {
+
+//     let id = "nz.school.OnslowCollege.Reversi"
+//     var app: GTUIApp!
+
+//     var scene: Scene {
+//         Window(id: "main") { window in
+//             Text("NotReversi")
+//                 .padding()
+//                 .topToolbar {
+//                     ToolbarView(app: app, window: window)
+//                 }
+//         }
+//         .defaultSize(width: 450, height: 300)
+//     }
+
+// }
+//
+//  DiceDemo.swift
+//  Adwaita
+//
+//  Created by david-swift on 12.10.23.
+//
+
+// swiftlint:disable missing_docs no_magic_numbers
 
 import Adwaita
 
-@main
-struct Reversi: App {
+struct DiceDemo: View {
 
-    let id = "nz.school.OnslowCollege.Reversi"
-    var app: GTUIApp!
+    @State private var number: Int?
 
-    var scene: Scene {
-        Window(id: "main") { window in
-            Text("NotReversi")
-                .padding()
-                .topToolbar {
-                    ToolbarView(app: app, window: window)
-                }
+    private var label: String {
+        if let number {
+            return "\(number)"
+        } else {
+            return "Roll the Dice!"
         }
-        .defaultSize(width: 450, height: 300)
+    }
+
+    var view: Body {
+        VStack {
+            Button(label) {
+                number = .random(in: 1...6)
+            }
+            .pill()
+            .suggested()
+            .style("dice-button")
+            .css {
+                """
+                .dice-button {
+                    background-color: @green_5;
+                }
+                """
+            }
+            .frame(maxWidth: 100)
+        }
+        .valign(.center)
+        .padding()
     }
 
 }
+
+// swiftlint:enable missing_docs no_magic_numbers
