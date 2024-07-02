16163799b8aec0ebdba0871c2d09e7a9ac2d9ecc
AidanJWheeler <124639958+AidanJWheeler@users.noreply.github.com>
2024-07-02 02:33:56 +0000

    Jul 2, 2024, 2:33 AM

--- trial/src/routes/+page.svelte
+++ trial/src/routes/+page.svelte
@@ -4,4 +4,25 @@
     type State = 'start' | 'playing' | 'paused' | 'won' | 'lost'
 
     let state: State = 'start'
+    let size = 20
+    let grid = createGrid()
+
+    function createGrid() {
+        let cards = new Set<string>()
+        let maxSize = size / 2
+
+        while (cards.size < maxSize) {
+            const randomIndex = Math.floor(Math.random() * emoji.length)
+            cards.add(emoji[randomIndex])
+        }
+
+        return shuffle([...cards, ...cards])
+    }
+
+    function shuffle<Items>(array: Items[]) {
+        return array.sort(() => Math.random() - 0.5 )
+
+    }
+
+    console.log(grid)
 </script>
