c3727f67f6bbc27509ef6bffe230fdee030785f5
Annabelle <annabelle.leach@student.onslow.school.nz>
2024-07-21 23:47:24 +0000

    11:47 am

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,9 @@
+// Important bits
 import Fluent
 import Vapor
 
+
+/// the site itself I think
 func routes(_ app: Application) throws {
     app.get { req async throws in
         try await req.view.render("index", ["title": "Hello Vapor!"])
@@ -10,6 +13,11 @@ func routes(_ app: Application) throws {
         "Hello, world!"
     }
 
+    app.get("login") { req async -> String in
+        "This is where you will log in wooooo!"
+    }
 
     try app.register(collection: TodoController())
 }
+
+
