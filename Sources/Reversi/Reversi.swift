bf8ab489327963faf5b4fdcd0ee80f5cbabd1cd6
KellanVK <kellan.vankoughnet@student.onslow.school.nz>
2024-08-29 02:54:00 +0000

    Aug 29, 2024, 2:54 AM

--- Sources/Reversi/Reversi.swift
+++ Sources/Reversi/Reversi.swift
@@ -559,7 +559,7 @@ class ReversiApp : OCApp {
         let ControlRow = OCHBox(controls: [RulesButton, ResetButton, ModeDropDown, colourSwitch])
         let Program = OCVBox(controls: [pieceCountLabel, winnerStatusLabel, Grid, ControlRow])
         let ProgramFull = OCVBox(controls: [Program, rules])
-        Grid.setStyle(OCStyle.backgroundColor(OCColor.gray))
+        Grid.setStyle(OCStyle.backgroundColor(OCColor.chatreuse))
         rules.visible = false
         return ProgramFull
     }
